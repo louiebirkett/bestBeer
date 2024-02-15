3aaa4064e94c65f9c9d086bd6f16682fe7d9073f
AlexOakley123 <127768446+AlexOakley123@users.noreply.github.com>
2024-02-15 16:27:20 +0000

    Initial connection to database
    Example querys

--- backend/index.php
+++ backend/index.php
@@ -19,12 +19,29 @@ function connectToMysql() {
     return $conn;
 }
 
+print_r("<h1>Hello from Alex<h1>");
 $conn = connectToMysql();
 
-$sql = "SELECT * FROM test";
-$result = $conn->query($sql);
-while($row = $result->fetch_assoc()) {
-    print_r($row);
+$result = mysqli_query($conn, "SELECT * FROM tableBeerTypes WHERE name = 'Stella'");
+// get result as associative array
+$row = mysqli_fetch_assoc($result);
+// print result
+/*
+echo  ' Name '. $row['name'] . ' Alc percentage '. $row['alcPercent'];
+
+$st = $conn->prepare("DELETE FROM `tableBeerTypes` WHERE name = 'Stella'");
+$st->execute();
+*/
+$sql = "INSERT INTO tableBeerTypes (name, alcPercent, linkToImg)
+VALUES ('Stella', '4.9', 'images/stella.png')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
 }
+
+
+
 $conn->close();
 ?>
