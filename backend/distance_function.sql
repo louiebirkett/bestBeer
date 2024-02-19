a22b70789a3f31016f5f222db61ae1fc83208e55
michqql <mjpearson100@gmail.com>
2024-02-19 23:13:01 +0000

    MySQL distance function index.php now returns pubs within a radius of a supplied lat and long location.

---
+++ backend/distance_function.sql
@@ -0,0 +1,17 @@
+/* Change delimiter so that MySQL does not think the statement is over when using ; */
+DELIMITER &&
+
+/* Algorithm from: https://enlear.academy/working-with-geo-coordinates-in-mysql-5451e54cddec */
+CREATE OR REPLACE FUNCTION dist (posLat DECIMAL(35, 30), posLong DECIMAL(35, 30), userLat DECIMAL(35, 30), userLong DECIMAL(35, 30)) 
+RETURNS DECIMAL(35, 30)
+DETERMINISTIC
+BEGIN
+    DECLARE DIST_M DECIMAL(35, 30);
+    SET DIST_M = 6371000 * Acos(Cos(Radians(userLat)) * Cos(Radians(posLat)) * 
+                                        Cos(Radians(posLong) - Radians(userLong)) + 
+                                        Sin(Radians(userLat)) * Sin(Radians(posLat)));
+    RETURN DIST_M;
+END&&
+
+/* Change delimiter back to default */
+DELIMITER ;
--- backend/index.php
+++ backend/index.php
@@ -19,14 +19,39 @@ function connectToMysql() {
     return $conn;
 }
 
+function errorResponse($code, $message) {
+    http_response_code($code);
+    echo "{\"message\":\"$message\"}";
+}
+
+// Check if the request contains the user lat & long values to process this request
+if(!isset($_POST['userLat']) || !isset($_POST['userLong'])) {
+    // Return error status code
+    errorResponse(400, 'Request must contain userLat and userLong parameters for current location.');
+    return;
+}
+
+// Get the values
+$userLat = $_POST['userLat'];
+$userLong = $_POST['userLong'];
+$searchRadius = 5000; // 5km / 5000m
+if(isset($_POST['searchRadius'])) {
+    $searchRadius = $_POST['searchRadius'];
+}
+
+// TODO: Sanatise user lat, long, search radius values before putting into sql statement
+
+// Connect to the MySQL database
 $conn = connectToMysql();
 
-$sql = "SELECT * FROM tableBeerTypes";
+// Search for pubs within a certain distance around the location
+$sql = "SELECT name, dist(locLat, locLong, $userLat, $userLong) AS dist FROM tablePubs HAVING dist < $searchRadius;";
 $result = $conn->query($sql);
 $arr = array();
 while($row = $result->fetch_assoc()) {
-    $element = array("name" => $row["name"], "alcPercent" => $row["alcPercent"]);
-    array_push($arr, $element);
+    //$element = array("name" => $row["name"], "alcPercent" => $row["alcPercent"]);
+    //array_push($arr, $element);
+    print_r($row);
 }
 $conn->close();
 
