7897a546238e8e89258ba5e36e8ccf61df1ce18a
Louie <116655424+louiebirkett@users.noreply.github.com>
2024-02-20 18:28:30 +0000

    Update sidebar.js
    Made Search bar a React Components

--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -1,15 +1,25 @@
 import '../styles/styles.css'
 function SideBar(){
     return(
+        
         <div className='sidebarContainer'>
-            <search>
-                <form className='searchBar'>
-                    <input type="search" placeholder='Search for a Pub'></input>
-                </form>
-            </search>
+            <SearchBar/>
         </div>
     )
 
 }
+
+
+
+function SearchBar(){
+    return (    
+    <search>
+        <form className='searchBar'>
+            <input type="search" placeholder='Search for a Pub'></input>
+        </form>
+    </search>)
+ 
+}
+
 
 export default SideBar;
