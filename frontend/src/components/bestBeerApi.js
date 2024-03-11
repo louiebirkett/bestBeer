43197711fdd48f8325911be1610203bc24441283
AlexOakley123 <127768446+AlexOakley123@users.noreply.github.com>
2024-03-11 17:42:43 +0000

    updated to use then

--- frontend/src/components/bestBeerApi.js
+++ frontend/src/components/bestBeerApi.js
@@ -1,31 +1,29 @@
 
 
-async function populateObject(userLat, userLong, searchRadius) {
-    
+function populateObject(userLat, userLong, searchRadius) {
     const url = 'https://mp1282.brighton.domains/ci536/index.php?userLat=' + userLat + '&userLong=' + userLong + '&searchRadius=' + searchRadius;
 
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error('Unable to connect to Api');
-        }
-        const data = await response.json();
-
-        const pubs = data.map(item => ({
-            name: item.name,
-            distance: item.dist
-        }));
-
-        console.log(pubs);
-
-        
-        return pubs;
-
-    } catch (error) {
-        console.error('There has been a problem with the fetch operation:', error);
-        return null; // Or handle the error appropriately
-    }
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Unable to connect to Api');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const pubs = data.map(item => ({
+                name: item.name,
+                distance: item.dist
+            }));
+            console.log(pubs);
+            return pubs;
+        })
+        .catch(error => {
+            console.error('There has been a problem with the fetch operation:', error);
+            return null; // Or handle the error appropriately
+        });
 }
+
 
 
 export default populateObject;
