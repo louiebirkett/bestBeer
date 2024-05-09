d8776375b96d26d52b4066cab7051dd0d0d2c1a4
AlexOakley123 <127768446+AlexOakley123@users.noreply.github.com>
2024-05-09 16:58:28 +0100

    Stars css
    Implemented a system that makes the star rating show as a stars

--- frontend/src/components/resultwidget.js
+++ frontend/src/components/resultwidget.js
@@ -50,12 +50,19 @@ function googlePriceLevelToSymbol(priceLevel) {
     }
 }
 
+function getRatingStars(rating){
+    let result =  100 - (rating / 5) * 27.25;
+    return result + "%";
+     
+}
+
 function ResultWidget({pubObject}){
     let distance = (pubObject.distance / 1000).toFixed(2);
     let price = googlePriceLevelToSymbol(pubObject.price);
 
     return (
     <div className="resultWrapper">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
         <h1 className='resultTitle' style={{fontSize: "larger"}}>{pubObject.name}</h1>
         <div className='partition'>
             <svg viewBox="0 0 415 36" fill="none" xmlns="http://www.w3.org/2000/svg"> <line y1="0.5" x2="415" y2="0.5" stroke="white"/> <line x1="270.5" y1="10" x2="270.5" y2="36" stroke="white"/></svg>
@@ -84,7 +91,25 @@ function ResultWidget({pubObject}){
                 </tr>
             </table> */}
         </div>
-        <h1 className="resultRating">{pubObject.rating}&#9733;</h1>
+        <div>
+
+        <div class="stars">
+            <svg viewBox="0 0 576 512" width="100" title="star">
+            <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
+            </svg><svg viewBox="0 0 576 512" width="100" title="star">
+            <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
+            </svg><svg viewBox="0 0 576 512" width="100" title="star">
+            <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
+            </svg><svg viewBox="0 0 576 512" width="100" title="star">
+            <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
+            </svg><svg viewBox="0 0 576 512" width="100" title="star">
+            <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
+            </svg>
+
+            <div class="cover" style={{ width: getRatingStars(pubObject.rating)  }}></div>
+        </div>
+
+</div>
     </div>
     );
 };
